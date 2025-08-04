4f6b4602f2b869b9c72d0f44e633a37b584345a5
behan02 <behancrick@gmail.com>
2025-08-04 13:52:18 +0530

    Create the signup function

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,18 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js";
+
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000!");
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
