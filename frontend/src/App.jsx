53103dbade943947c14404ba9f4c795cc547f25f
behan02 <behancrick@gmail.com>
2025-08-14 09:21:34 +0530

    Add cors to the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,18 @@ import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 const App = () => {
+  const { authUser, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  },[checkAuth])
+
+  console.log({ authUser });
+
   return (
     <div>
       <Navbar />
