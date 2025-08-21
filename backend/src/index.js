90d0552ecbd82df1751532f62c6f916a40281784
behan02 <behancrick@gmail.com>
2025-08-21 11:55:20 +0530

    Update cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,10 +12,10 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true
 }))
 
