b6f88576640ac369facbd84fc6466bb230d02a58
behan02 <behancrick@gmail.com>
2025-08-22 18:09:20 +0530

    Change the color of message skeletons

--- frontend/src/components/skeletons/MessageSkeleton.jsx
+++ frontend/src/components/skeletons/MessageSkeleton.jsx
@@ -8,16 +8,16 @@ const MessageSkeleton = () => {
         <div key={idx} className={`chat ${idx % 2 === 0 ? "chat-start" : "chat-end"}`}>
           <div className="chat-image avatar">
             <div className="size-10 rounded-full">
-              <div className="skeleton w-full h-full rounded-full" />
+              <div className="w-full h-full rounded-full bg-gray-300 animate-pulse" />
             </div>
           </div>
 
           <div className="chat-header mb-1">
-            <div className="skeleton h-4 w-16" />
+            <div className="h-4 w-16 bg-gray-300 rounded animate-pulse" />
           </div>
 
           <div className="chat-bubble bg-transparent p-0">
-            <div className="skeleton h-16 w-[200px]" />
+            <div className="h-16 w-[200px] bg-gray-300 rounded animate-pulse" />
           </div>
         </div>
       ))}
